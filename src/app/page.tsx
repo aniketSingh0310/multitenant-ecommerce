fb918a09715352684b91478b1ae660d1886a2de7
aniketSingh0310 <aniketfcb10@gmail.com>
2025-04-25 18:53:22 +0530

    feat:customisation chapt 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,31 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+
 export default function Home() {
   return (
-    <div>
-      <p className="text-red-500">HI WORLD</p>
+   <div className="p-5 ">
+   <div className="flex flex-col gap-y-4">
+      <div>
+        <Button variant="elevated">Button</Button>
+      </div>
+
+      <div>
+        <Input placeholder="Input bar" />
+      </div>
+      <div>
+        <Progress value={50} />
+      </div>
+      <div>
+        <Textarea placeholder="Textarea" />
+      </div>
+      <div>
+        <Checkbox id="checkbox" />
+        <label htmlFor="checkbox">Checkbox</label>
+      </div>
+    </div>
     </div>
   );
 }
