434c4e36830144d9269a563ee6e18fedb4676f59
aniketSingh0310 <aniketfcb10@gmail.com>
2025-05-23 15:22:43 +0530

    fix:superjson

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -6,12 +6,12 @@ import { useQuery } from "@tanstack/react-query";
 export default  function Home() {
 
   const trpc= useTRPC();
-  const categories=useQuery(trpc.categories.getMany.queryOptions())
+  const {data}=useQuery(trpc.auth.session.queryOptions())
   return (
    
 <div>
-  {categories.isLoading && <p>Loading...</p>}
-  {JSON.stringify(categories.data,null, 2)}
+  
+  {JSON.stringify(data,null, 2)}
 </div>
   );
 }
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,12 @@
 
 import { createTRPCRouter } from '../init';
+
+import { authRouter } from '@/modules/auth/server/procedures';
 import { categoriesRouter } from '@/modules/categories/server/procedures';
 
 export const appRouter = createTRPCRouter({
-  categories:categoriesRouter
+  categories:categoriesRouter,
+  auth:authRouter
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
